c3a89b4b90d83999fe7540e523a44419f51c4c05
Nikolay Orlov <nik-or@mail.ru>
2024-08-10 15:27:37 +0600

    restful

---
+++ 07_rest/order-service/src/main/java/homework/arch/orderservice/OrderServiceApplication.java
@@ -0,0 +1,11 @@
+package homework.arch.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+}
