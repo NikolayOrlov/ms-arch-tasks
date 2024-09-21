66afce9e505524ab640eef0852f8ef3071d18cf5
Nikolay Orlov <nik-or@mail.ru>
2024-09-21 22:52:25 +0600

    idempotent API - applications

---
+++ 09_idempotency/checkout-service/src/main/java/homework/arch/checkoutservice/persistence/CheckoutEntity.java
@@ -0,0 +1,49 @@
+package homework.arch.checkoutservice.persistence;
+
+import homework.arch.checkoutservice.client.cart.dto.generated.CartDto;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CheckoutEntity {
+    @Id
+    @UuidGenerator(style = UuidGenerator.Style.AUTO)
+    private UUID id;
+
+    private UUID customerId;
+
+    private UUID orderId;
+
+    private UUID cartId;
+
+    private BigDecimal amount;
+
+    private PaymentStatus status;
+
+    private LocalDateTime requestedTimestamp;
+
+    private LocalDateTime confirmedTimestamp;
+
+    public enum PaymentStatus {
+        PENDING, DONE, FAILED
+    }
+
+    @Transient
+    private CartDto cartDto;
+}
